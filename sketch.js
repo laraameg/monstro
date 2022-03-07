fe46cd5137f32e7f895807d598cefc4427267a90
laraameg <92958032+laraameg@users.noreply.github.com>
2022-03-07 20:00:51 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, box2, box3,box4;
+var hero,monster,rope,ground;
+var boxes = []
+
+function preload() {
+  bg = loadImage("gamingbackground2.png");
+}
+
+function setup() {
+  createCanvas(3000, 700);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(600, 600, 1200, 20);
+
+  hero = new Hero(400,800,250);
+  rope = new Rope(hero.body, { x: 500, y: 50 });
+  monster = new Monster(1100,550,300);
+
+  box1 = new Box(900, 100, 70, 70);
+  box2 = new Box(900, 100, 70, 70);
+  box3 = new Box(900, 100, 70, 70);
+  box4 = new Box(900, 100, 70, 70);
+  for (var j = 100; j <= 400; j = j + 70) {
+    boxes.push(new Box(600, j, 70, 70));
+  }
+
+
+
+}
+
+function draw() {
+  background(bg);
+  Engine.update(engine);
+  ground.display();
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display();
+
+  hero.display();
+  rope.display();
+  monster.display();
+
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].display();
+  }
+
+
+}
+
+function mouseDragged() {
+  Matter.Body.setPosition(hero.body, {x:mouseX, y: mouseY});
+}
+
+function mouseReleased() {
+    rope.fly();
+  }
+
+
